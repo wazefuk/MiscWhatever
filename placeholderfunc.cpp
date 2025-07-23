cb649914ade9e57c7951cae46e3eabe6c20d44db
Wazefuk <112293501+wazefuk@users.noreply.github.com>
2025-07-23 17:14:11 -0500

    placeholderfunc.cpp notices

--- placeholderfunc.cpp
+++ placeholderfunc.cpp
@@ -1,13 +1,20 @@
+#include <iostream>
 #include "CMD.hh"
 
 void CMD::CMD_run::AHiddenCommandNotShownInHelp_Main() {
-    return;
+    std::cout << "\033[31m"; 
+    std::cout << "\nNotice: CMD::CMD_run::AHiddenCommandNotShownInHelp_Main() is currently unimplemented" << std::endl;
+    std::cout << "\033[0m";
 }
 
 void CMD::CMD_run::CMDHistory_Main() {
-    return;
+    std::cout << "\033[31m"; 
+    std::cout << "\nNotice: CMD::CMD_run::CMDHistory_Main() is currently unimplemented" << std::endl;
+    std::cout << "\033[0m";
 }
 
 void CMD::CMD_run::SHOWTHEGITIGNORE_Main() {
-    return;
+    std::cout << "\033[31m"; 
+    std::cout << "\nNotice: CMD::CMD_run::SHOWTHEGITIGNORE_Main() is currently unimplemented" << std::endl;
+    std::cout << "\033[0m";
 }
